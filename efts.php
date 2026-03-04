8e76f8e5f47100971d75bef933362a3b01cef843
Ravail <ravail.irshad@ccjk.com>
2026-03-04 16:11:20 +0500

    php removal from files

--- efts.php
+++ efts.php
@@ -1,4 +1,16 @@
 <?php
+// Handle clean URLs - extract filter from URL path if not in query string
+if (!isset($_GET['filter']) && isset($_SERVER['REQUEST_URI'])) {
+    $request_uri = $_SERVER['REQUEST_URI'];
+    $path = parse_url($request_uri, PHP_URL_PATH);
+    $path_parts = explode('/', trim($path, '/'));
+    $last_segment = end($path_parts);
+    
+    if ($last_segment === 'indexes') {
+        $_GET['filter'] = 'eft';
+    }
+}
+
 include "db.php";
 if(isset($_POST['setnotification'])){
     $useremail=$_POST['useremail'];
@@ -272,7 +284,7 @@ function closeTradingView() {
                     // Create a form
                     var form = document.createElement("form");
                     form.method = "POST";
-                    form.action = "favorite.php?filter=favorites";
+                    form.action = "favorites";
 
                     // Create a hidden input field for favorites
                     var input = document.createElement("input");
