705fcb98bc8b7fb8e8145710f32c04a820eaecd0
Ravail <ravail.irshad@ccjk.com>
2026-03-06 15:46:19 +0500

    fix footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <div class="container">
 <div class="row pt-4">
 	<div class="col-md-4">
-		<p class="text">
+		<p class="text" data-nosnippet>
 		Welcome to Correction Territory, your premier online destination for exploring and investing in the stock market. Whether you are an experienced trader or just beginning your investment journey, we provide simple indicators to help you make informed and easy decisions.
 		</p>
 	</div>
