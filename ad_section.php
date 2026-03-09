d4e6a9812fe4177386645bdc509ba6c6d078a60a
Ravail <ravail.irshad@ccjk.com>
2026-03-09 13:14:33 +0500

    second slot

--- ad_section.php
+++ ad_section.php
@@ -24,7 +24,7 @@
     <ins class="adsbygoogle"
          style="display:block"
          data-ad-client="ca-pub-8754771874266985"
-         data-ad-slot="7225743774"
+         data-ad-slot="2910158078"
          data-ad-format="auto"
          data-full-width-responsive="true"></ins>
     <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
