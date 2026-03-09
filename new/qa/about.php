aeca17a026b1dd3e02041439ae1aa909baf597cc
Ravail <ravail.irshad@ccjk.com>
2026-03-09 13:38:51 +0500

    fixes

--- new/qa/about.php
+++ new/qa/about.php
@@ -1,6 +1,139 @@
 <?php include "head.php"; ?>
 <?php include "header.php" ?>
 
+<style>
+    
+    .bg-warning {
+        background-color: #f5be19 !important;
+    }
+    .mynumber {
+    font-size: 13px !important;
+    height: 15px !important;
+    width: 30px !important;
+    border: 1px solid white !important;
+    box-shadow: 1px 1px 1px 1px white !important;
+    border-bottom: 1px solid black !important;
+    margin-top: -1px !important;
+    padding: 5px !important;
+}
+    .bg-danger, .bg-success, .bg-warning {
+        color: white !important;
+        font-weight: 500;
+    }
+    thead {
+        font-size: 13px;
+    }
+    table {
+        text-align: center;
+    }
+    .cname1 {
+        text-align: left !important;
+        vertical-align: middle !important;
+    }
+    .cname {
+        text-align: left !important;
+    position: relative; /* Establish a positioning context for absolute positioning */
+    padding-right: 20px; /* Add padding to the right for the icon */
+}
+
+.heart-icon {
+    position: absolute; /* Position the heart icon absolutely */
+    right: 3px; /* Align it to the right of the cell */
+    top: 50%; /* Center vertically */
+    transform: translateY(-50%); /* Adjust to perfect vertical centering */
+    color: red; /* Optional: Set the color of the heart icon */
+}
+
+    .table-striped tbody tr:nth-of-type(odd) {
+            background-color: #E6E6B1;
+        }
+    table.dataTable tbody tr {
+        background-color: #FFFFC5;
+    }
+    table.dataTable thead th, table.dataTable thead td {
+            padding: 9px 2px;
+            border-bottom: 1px solid #111;
+        }
+    
+    table.dataTable thead .sorting {
+        background-image:none;
+    }    
+    .table-striped tbody tr:nth-of-type(odd) {
+        background-color: #FFFFC5;
+    }  
+      
+        
+    @media (max-width: 768px) {
+        .hidemobile {
+            display: none;
+        }
+        .customheading{
+            padding-left:10px;
+            font-size:19px;
+        }
+            .mynumber {
+        font-size: 13px !important;
+        height: 13px !important;
+        width: 30px !important;
+        border: 1px solid white !important;
+        box-shadow: 1px 1px 1px 1px white !important;
+        border-bottom: 1px solid black !important;
+        margin-top: -3px !important;
+        padding: 5px !important;
+    }
+        thead, table {
+            font-size: 10px;
+        }
+        
+        table.dataTable thead th {
+            padding-right: 15px; /* Add padding to the right of header cells */
+        }
+        
+        table.dataTable thead th.sorting:after, 
+        table.dataTable thead th.sorting_asc:after, 
+        table.dataTable thead th.sorting_desc:after {
+            margin-left: 5px; /* Adjust the margin between text and arrow */
+        }
+        
+    }
+.table-striped>tbody>tr:nth-of-type(odd)>* {
+--bs-table-accent-bg: rgb(0 0 0 / 0%)!important;
+border-bottom: 1px solid #e7e7e7;
+color: var(--bs-table-striped-color);
+text-align: center;
+}
+table.dataTable tbody th, table.dataTable tbody td {
+border-bottom: 1px solid #e7e7e7;
+
+color: var(--bs-table-striped-color);
+text-align: center;
+}
+
+.text{
+    color: #f1f1f1;
+    text-decoration:none;
+}
+.text:hover{
+    color: #ffffff;
+    cursor: pointer;
+    text-decoration:none;
+}
+.footer-ul{
+    list-style: none;
+}
+.footer-ul>li{
+    padding-top: 12px;
+    border-bottom: 1px solid #ffffff3b;
+    padding-bottom: 3px;
+}
+h2{
+    font-size:18px;
+}
+.mt-6 {
+    margin-top: 5rem !important;
+}
+</style>
+
 <div class="container" style="padding:50px 5px;">
     <h1 class="mt-6">About Us</h1>
     <p>Welcome to Correction Territory, your trusted online resource for in-depth analysis and insights into the S&P 500 stocks. Our platform is designed to provide you with essential financial data at a glance, including company market capitalization, current stock prices, historical all-time highs, and detailed correction ranges. Whether you're an experienced investor or just starting out, we aim to simplify the complexities of the stock market, offering you the tools and information needed to make informed investment decisions with confidence.</p>
@@ -8,12 +141,90 @@
     <br/>
 </div>
 
-<style>
-.mt-6 {
-    margin-top: 5rem !important;
+<style type="text/css">
+    .footer-wrapper {
+    padding: 0px;
+    background-color: #333333;
+    color: #fff;
 }
+
+.footer-widgets {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.footer-widgets .widget {
+    flex: 1 1 200px; /* Flex-grow, flex-shrink, flex-basis */
+    margin: 10px;
+}
+
+.widget-title {
+    font-size: 1.2em;
+    margin-bottom: 10px;
+}
+
+.is-divider {
+    margin: 10px 0;
+}
+
+.footer .col-inner {
+    margin: 0;
+    padding: 0;
+}
+
+.footer-primary {
+    text-align: center;
+    margin-top: 20px;
+}
+
+@media (max-width: 767px) {
+    .footer-widgets {
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .footer-widgets .widget {
+        width: 100%;
+        max-width: 400px;
+    }
+
+    .widget-title {
+        text-align: center;
+    }
+}
+
+@media (max-width: 480px) {
+    .footer-widgets .widget {
+        padding: 10px;
+    }
+
+    .widget-title {
+        font-size: 1em;
+    }
+
+    .is-divider {
+        margin: 5px 0;
+    }
+}
+
+.absolute-footer {
+    text-align: center;
+    padding: 7px 0;
+    background-color: #111;
+}
+
+.back-to-top {
+    display: none; /* Initially hidden */
+}
+
+@media (min-width: 768px) {
+    .back-to-top {
+        display: block;
+    }
+}
+
 </style>
 
 <?php include "footer.php"; ?>
 <?php include "body_end.php"; ?>
-
